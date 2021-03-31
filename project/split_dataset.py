353df6a8070e899ea4bcac16eaf2114c573d571e
Saqib Ameen <mrsaqibameen@gmail.com>
2021-03-31 00:24:33 -0600

    update

--- project/split_dataset.py
+++ project/split_dataset.py
@@ -1,5 +1,4 @@
 import json
-from pprint import pprint
 
 ROOT_DIRECTORY = './../'
 DATASET_ABS_PATH = 'dataset/sstubs.txt'
@@ -17,7 +16,5 @@ for project in project_names:
     data_for_project = list(filter(lambda bug: bug[PROJECT_NAME] == project, dataset))
     group_by_project[project] = data_for_project
     project_file = open(ROOT_DIRECTORY + DATASET_BY_PROJECT_ABS_PATH + project, 'w', encoding="ascii")
-    # project_file.write()
     json.dump(data_for_project, project_file, indent = 2)
-    # pprint(json.dumps(data_for_project), stream=project_file)
     project_file.close()
